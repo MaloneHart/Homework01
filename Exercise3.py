e31519daed74840d7ca0d1e22ad2b05cac9af616
MaloneHart <109078288+MaloneHart@users.noreply.github.com>
2022-07-19 17:47:20 +0400

    Add files via upload

---
+++ Exercise3.py
@@ -0,0 +1,27 @@
+# Задача-3: Напишите программу, вычисляющую корни квадратного уравнения вида
+# ax² + bx + c = 0.
+# Коэффициенты уравнения вводятся пользователем.
+# Для вычисления квадратного корня воспользуйтесь функцией sqrt() модуля math:
+# import math
+# math.sqrt(4) - вычисляет корень числа 4
+
+import math
+
+print("Введите коэффициенты")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+discr = b ** 2 - 4 * a * c
+print("Дискриминант D = %.2f" % discr)
+
+if discr > 0:
+    x1 = (-b + math.sqrt(discr)) / (2 * a)
+    x2 = (-b - math.sqrt(discr)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discr == 0:
+    x = -b / (2 * a)
+    print("x = %.2f" % x)
+else:
+    print("Корней нет")
